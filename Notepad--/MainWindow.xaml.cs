86f1691dfc1203b45463bbe4fb9e1707e20782ad
brapstu <minecrafter2757@gmail.com>
2022-06-25 10:11:47 -0500

    v1.1.0 update. added save, added some dialog... or dialogue if ur weird

--- Notepad--/MainWindow.xaml.cs
+++ Notepad--/MainWindow.xaml.cs
@@ -27,13 +27,29 @@ namespace Notepad__
             InitializeComponent();
         }
 
+        public string OpenFile = "C:/Untitled.txt";
+        public string CurrentFile = "Untitled";
+        public bool FileChanged = false;
+
 
         private void NewClicked(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result = MessageBox.Show("Are you sure you want to create a new document?", "Confirmation", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
-            if (result == MessageBoxResult.OK)
-            {
+            if (TextBox.Text == "" || TextBox.Text == "Type here" || FileChanged == false) {
                 TextBox.Text = String.Empty;
+                CurrentFile = "Untitled";
+                OpenFile = "";
+                FileChanged = false;
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to create a new document?", "Confirmation", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.OK)
+                {
+                    TextBox.Text = String.Empty;
+                    CurrentFile = "Untitled";
+                    OpenFile = "";
+                    FileChanged = false;
+                }
             }
         }
 
@@ -42,7 +58,7 @@ namespace Notepad__
             Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
             dlg.FileName = "Document"; // Default file name
             dlg.DefaultExt = ".txt"; // Default file extension
-            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+            dlg.Filter = "All Files|*.*"; // Filter files by extension
 
             // Show save file dialog box
             bool? result = dlg.ShowDialog();
@@ -54,15 +70,95 @@ namespace Notepad__
                 TextWriter tw = new StreamWriter(dlg.FileName);
                 tw.WriteLine(TextBox.Text);
                 tw.Close();
+                OpenFile = dlg.FileName;
+                CurrentFile = System.IO.Path.GetFileName(OpenFile);
+                MainWindow1.Title = CurrentFile + " : Notepad--";
+                FileChanged = false;
             }
 
         }
 
+        
         private void OpenClicked(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
                 TextBox.Text = File.ReadAllText(openFileDialog.FileName);
+                OpenFile = openFileDialog.FileName;
+                CurrentFile = System.IO.Path.GetFileName(OpenFile);
+                MainWindow1.Title = CurrentFile + " : Notepad--";
+                FileChanged = false;
+        }
+
+        private void SettingsClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This feature will be added soon.");
+        }
+
+        private void OnWindowClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (FileChanged == true)
+            {
+                MessageBoxResult result = MessageBox.Show("Do you want to save changes to " + CurrentFile + "?", "Unsaved Changes", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    TextWriter tw = new StreamWriter(OpenFile);
+                    tw.WriteLine(TextBox.Text);
+                    tw.Close();
+                    FileChanged = false;
+                    e.Cancel = true;
+                }
+
+                if (result == MessageBoxResult.No)
+                {
+                    // Do nothin :p
+                }
+            }
+            else
+            {
+                // DO NOTHIN AGAIN!!!! caus u know
+            }
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            FileChanged = true;
+            MainWindow1.Title = "*" + CurrentFile + " : Notepad--";
+        }
+
+        private void SaveTClicked(object sender, RoutedEventArgs e) // Why did i call it saveT
+        {
+            if (CurrentFile == "Untitled")
+            {
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.FileName = "Document"; // Default file name
+                dlg.DefaultExt = ".txt"; // Default file extension
+                dlg.Filter = "All Files|*.*"; // Filter files by extension
+
+                // Show save file dialog box
+                bool? result = dlg.ShowDialog();
+
+                // Process save file dialog box results
+                if (result == true)
+                {
+                    // Save document
+                    TextWriter tw = new StreamWriter(dlg.FileName);
+                    tw.WriteLine(TextBox.Text);
+                    tw.Close();
+                    OpenFile = dlg.FileName;
+                    CurrentFile = System.IO.Path.GetFileName(OpenFile);
+                    MainWindow1.Title = CurrentFile + " : Notepad--";
+                    FileChanged = false;
+                }
+            }
+            else
+            {
+                TextWriter tw = new StreamWriter(OpenFile);
+                tw.WriteLine(TextBox.Text);
+                tw.Close();
+                MainWindow1.Title = CurrentFile + " : Notepad--";
+                FileChanged = false;
+            }
         }
     }
 }
